60c751d1d89ade240060fc388d4afd6b785afbd2
sakibbiswas <sakibsakib99880@gmail.com>
2023-07-12 16:37:34 +0600

    gitinore added

--- index.js
+++ index.js
@@ -25,10 +25,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        // Connect the client to the server	
         // await client.connect();
-
-
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
